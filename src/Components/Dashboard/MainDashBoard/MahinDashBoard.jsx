d40193ec6f028b4896399dce098d42d6c85ba2cf
originalmahim <tareqazizmahim@gmail.com>
2024-02-10 15:29:11 +0600

    Created Main dashboard Successfully

---
+++ src/Components/Dashboard/MainDashBoard/MahinDashBoard.jsx
@@ -0,0 +1,40 @@
+
+import CardDataStats from '../../components/CardDataStats';
+import ChartOne from '../../components/Charts/ChartOne';
+import ChartTwo from '../../components/Charts/ChartTwo';
+import ChatCard from '../../components/Chat/ChatCard';
+import TableOne from '../../components/Tables/TableOne';
+import DefaultLayout from '../../layout/DefaultLayout';
+
+const MahinDashBoard = () => {
+  return (
+    <DefaultLayout>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
+          <svg
+            className="fill-primary dark:fill-white"
+            width="22"
+            height="16"
+            viewBox="0 0 22 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {/* SVG Path content */}
+          </svg>
+        </CardDataStats>
+        {/* Repeat for other CardDataStats components */}
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <ChartOne />
+        <ChartTwo />
+        <div className="col-span-12 xl:col-span-8">
+          <TableOne />
+        </div>
+        <ChatCard />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default MahinDashBoard;
