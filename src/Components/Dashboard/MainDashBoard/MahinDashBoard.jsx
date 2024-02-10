fa862ee0a79dcbf38e8bae93176c467bce122a4e
originalmahim <tareqazizmahim@gmail.com>
2024-02-10 16:23:32 +0600

    Created Table successfully

--- src/Components/Dashboard/MainDashBoard/MahinDashBoard.jsx
+++ src/Components/Dashboard/MainDashBoard/MahinDashBoard.jsx
@@ -1,10 +1,8 @@
-// import ChartTwo from '../../components/Charts/ChartTwo';
-// import ChatCard from '../../components/Chat/ChatCard';
-// import TableOne from '../../components/Tables/TableOne';
 import DefaultLayout from '../DefaultLayout/DefaultLayout';
 import CardDataStats from './CardDataStats';
 import ChartOne from './ChartOne';
 import ChartTwo from './ChartTwo';
+import TableOne from './TableOne';
 
 
 
@@ -101,10 +99,10 @@ const MahinDashBoard = () => {
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        {/* <div className="col-span-12 xl:col-span-8">
+        <div className="col-span-12 xl:col-span-8">
           <TableOne />
         </div>
-        <ChatCard /> */}
+        {/* <ChatCard /> */}
       </div>
     </DefaultLayout>
   );
