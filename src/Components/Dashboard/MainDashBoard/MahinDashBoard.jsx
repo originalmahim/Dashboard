70f453fb50f6f47ea9d5c84f830831b38b9b1542
originalmahim <tareqazizmahim@gmail.com>
2024-02-15 13:51:43 +0600

    Updated Dashboard home page successfully

--- src/Components/Dashboard/MainDashBoard/MahinDashBoard.jsx
+++ src/Components/Dashboard/MainDashBoard/MahinDashBoard.jsx
@@ -11,7 +11,7 @@ const MahinDashBoard = () => {
   return (
     <DefaultLayout>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
+        <CardDataStats title="Total website views" total="3.456K" rate="0.43%" levelUp>
           <svg
             className="fill-primary dark:fill-white"
             width="22"
@@ -30,7 +30,7 @@ const MahinDashBoard = () => {
             />
           </svg>
         </CardDataStats>
-        <CardDataStats title="Total Profit" total="$45,2K" rate="4.35%" levelUp>
+        <CardDataStats title="Total Order Amount" total="৳ 45.2K" rate="4.35%" levelUp>
           <svg
             className="fill-primary dark:fill-white"
             width="20"
@@ -53,7 +53,7 @@ const MahinDashBoard = () => {
             />
           </svg>
         </CardDataStats>
-        <CardDataStats title="Total Product" total="2.450" rate="2.59%" levelUp>
+        <CardDataStats title="Total Product" total="24" rate="2.59%" levelUp>
           <svg
             className="fill-primary dark:fill-white"
             width="22"
@@ -72,7 +72,7 @@ const MahinDashBoard = () => {
             />
           </svg>
         </CardDataStats>
-        <CardDataStats title="Total Users" total="3.456" rate="0.95%" levelDown>
+        <CardDataStats title="Total Users" total="345">
           <svg
             className="fill-primary dark:fill-white"
             width="22"
