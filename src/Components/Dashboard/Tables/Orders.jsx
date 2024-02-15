91f9efe194385649bc48a63697f9683f0e4d9d94
originalmahim <tareqazizmahim@gmail.com>
2024-02-15 13:05:38 +0600

    Updated Manage Order table successfully

--- src/Components/Dashboard/Tables/Orders.jsx
+++ src/Components/Dashboard/Tables/Orders.jsx
@@ -1,26 +1,30 @@
-
+import { TbCoinTaka } from "react-icons/tb";
 const packageData = [
   {
-    name: 'Free package',
+    name: 'Abdul Wadud',
     price: 0.0,
+    number: '01882972481',
     invoiceDate: `Jan 13,2023`,
-    status: 'Paid',
+    status: 'Cash On Delivary',
   },
   {
-    name: 'Standard Package',
+    name: 'Jannatul Mim',
     price: 59.0,
+    number: '01882972481',
     invoiceDate: `Jan 13,2023`,
-    status: 'Paid',
+    status: 'Cash On Delivary',
   },
   {
-    name: 'Business Package',
+    name: 'Rakib Ul Alam',
     price: 99.0,
+    number: '01882972481',
     invoiceDate: `Jan 13,2023`,
     status: 'Unpaid',
   },
   {
-    name: 'Standard Package',
+    name: 'Sanjida Sultana',
     price: 59.0,
+    number: '01882972481',
     invoiceDate: `Jan 13,2023`,
     status: 'Pending',
   },
@@ -34,13 +38,16 @@ const Orders = () => {
           <thead>
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
               <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
-                Package
+                Details
+              </th>
+              <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
+                Number
               </th>
               <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                Invoice date
+                Order date
               </th>
               <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
-                Status
+               Payment Status
               </th>
               <th className="py-4 px-4 font-medium text-black dark:text-white">
                 Actions
@@ -54,7 +61,11 @@ const Orders = () => {
                   <h5 className="font-medium text-black dark:text-white">
                     {packageItem.name}
                   </h5>
-                  <p className="text-sm">${packageItem.price}</p>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <p className="text-black dark:text-white">
+                    {packageItem.number}
+                  </p>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <p className="text-black dark:text-white">
@@ -64,7 +75,7 @@ const Orders = () => {
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <p
                     className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${
-                      packageItem.status === 'Paid'
+                      packageItem.status === 'Cash On Delivary'
                         ? 'bg-success text-success'
                         : packageItem.status === 'Unpaid'
                         ? 'bg-danger text-danger'
@@ -76,7 +87,7 @@ const Orders = () => {
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <div className="flex items-center space-x-3.5">
-                    <button className="hover:text-primary">
+                    <button className="hover:text-primary text-black dark:text-white">
                       <svg
                         className="fill-current"
                         width="18"
@@ -95,7 +106,26 @@ const Orders = () => {
                         />
                       </svg>
                     </button>
-                    <button className="hover:text-primary">
+                    <button className="hover:text-primary text-black dark:text-white">
+                      <svg
+                        className="fill-current"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M16.8754 11.6719C16.5379 11.6719 16.2285 11.9531 16.2285 12.3187V14.8219C16.2285 15.075 16.0316 15.2719 15.7785 15.2719H2.22227C1.96914 15.2719 1.77227 15.075 1.77227 14.8219V12.3187C1.77227 11.9812 1.49102 11.6719 1.12539 11.6719C0.759766 11.6719 0.478516 11.9531 0.478516 12.3187V14.8219C0.478516 15.7781 1.23789 16.5375 2.19414 16.5375H15.7785C16.7348 16.5375 17.4941 15.7781 17.4941 14.8219V12.3187C17.5223 11.9531 17.2129 11.6719 16.8754 11.6719Z"
+                          fill=""
+                        />
+                        <path
+                          d="M8.55074 12.3469C8.66324 12.4594 8.83199 12.5156 9.00074 12.5156C9.16949 12.5156 9.31012 12.4594 9.45074 12.3469L13.4726 8.43752C13.7257 8.1844 13.7257 7.79065 13.5007 7.53752C13.2476 7.2844 12.8539 7.2844 12.6007 7.5094L9.64762 10.4063V2.1094C9.64762 1.7719 9.36637 1.46252 9.00074 1.46252C8.66324 1.46252 8.35387 1.74377 8.35387 2.1094V10.4063L5.40074 7.53752C5.14762 7.2844 4.75387 7.31252 4.50074 7.53752C4.24762 7.79065 4.27574 8.1844 4.50074 8.43752L8.55074 12.3469Z"
+                          fill=""
+                        />
+                      </svg>
+                    </button>
+                    <button className="hover:text-primary text-black dark:text-white">
                       <svg
                         className="fill-current"
                         width="18"
@@ -122,25 +152,7 @@ const Orders = () => {
                         />
                       </svg>
                     </button>
-                    <button className="hover:text-primary">
-                      <svg
-                        className="fill-current"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 18 18"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M16.8754 11.6719C16.5379 11.6719 16.2285 11.9531 16.2285 12.3187V14.8219C16.2285 15.075 16.0316 15.2719 15.7785 15.2719H2.22227C1.96914 15.2719 1.77227 15.075 1.77227 14.8219V12.3187C1.77227 11.9812 1.49102 11.6719 1.12539 11.6719C0.759766 11.6719 0.478516 11.9531 0.478516 12.3187V14.8219C0.478516 15.7781 1.23789 16.5375 2.19414 16.5375H15.7785C16.7348 16.5375 17.4941 15.7781 17.4941 14.8219V12.3187C17.5223 11.9531 17.2129 11.6719 16.8754 11.6719Z"
-                          fill=""
-                        />
-                        <path
-                          d="M8.55074 12.3469C8.66324 12.4594 8.83199 12.5156 9.00074 12.5156C9.16949 12.5156 9.31012 12.4594 9.45074 12.3469L13.4726 8.43752C13.7257 8.1844 13.7257 7.79065 13.5007 7.53752C13.2476 7.2844 12.8539 7.2844 12.6007 7.5094L9.64762 10.4063V2.1094C9.64762 1.7719 9.36637 1.46252 9.00074 1.46252C8.66324 1.46252 8.35387 1.74377 8.35387 2.1094V10.4063L5.40074 7.53752C5.14762 7.2844 4.75387 7.31252 4.50074 7.53752C4.24762 7.79065 4.27574 8.1844 4.50074 8.43752L8.55074 12.3469Z"
-                          fill=""
-                        />
-                      </svg>
-                    </button>
+                    
                   </div>
                 </td>
               </tr>
